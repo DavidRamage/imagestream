01cbd73296c3425ce8ae56bf2e7aeea3d30d5420
David Ramage <dramage@lsnetworks.net>
2015-11-03 17:36:16 -0800

    tidy up variables

--- udp_listen.py
+++ udp_listen.py
@@ -3,15 +3,15 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 import struct
 
-sock = socket(AF_INET, SOCK_DGRAM)
+SOCK = socket(AF_INET, SOCK_DGRAM)
 HOST = '255.255.255.255'
 PORT = 31337
 PKT_STRUCT = struct.Struct("> B l I H 457s")
-sock.bind((HOST, PORT))
+SOCK.bind((HOST, PORT))
 file_out = open("udp_rx.jpg", "wb")
 while True:
     (last_pkt, CRC_32, seq_num, bytes_sent, payload) =\
-        PKT_STRUCT.unpack(sock.recvfrom(512)[0])
+        PKT_STRUCT.unpack(SOCK.recvfrom(512)[0])
     for byte in payload:
         file_out.write(byte)
     if last_pkt == 1:
