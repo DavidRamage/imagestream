978c811362e9a2947741249d19bac3a7a378b04a
David Ramage <dramage@github.io>
2015-11-18 20:48:29 -0800

    fix issue #4

--- udp_listen.py
+++ udp_listen.py
@@ -18,25 +18,32 @@ try:
 except Exception as ex:
     syslog.syslog(syslog.LOG_ERR, "Unable to bind to socket due to %s" %\
     str(ex))
+try:
+    SOCK.setblocking(True)
+except Exception as ex:
+    raise
+
 BAD_PACKET = False
 NEXT_PACKET = 0
 while True:
-    (LAST_PKT, CRC_32, SEQ_NUM, bytes_sent, PAYLOAD) =\
+    (LAST_PKT, CRC_32, SEQ_NUM, BYTES_SENT, PAYLOAD) =\
         PKT_STRUCT.unpack(SOCK.recvfrom(512)[0])
     if SEQ_NUM == 0:
         BAD_PACKET = False
         IMG_BUFFER = BytesIO()
     if NEXT_PACKET != SEQ_NUM:
         BAD_PACKET = True
-        syslog.syslog(syslog.LOG_DEBUG, "Missed packet sequence number")
+        NEXT_PACKET = 0
+        syslog.syslog(syslog.LOG_DEBUG, "Missed packet sequence number."\
+        + "Expected %s, got %s" % (NEXT_PACKET, SEQ_NUM))
     if binascii.crc32(PAYLOAD) != CRC_32:
         BAD_PACKET = True
         syslog.syslog(syslog.LOG_DEBUG, "Bad PAYLOAD CRC in packet")
-    if BAD_PACKET == False:
-        for i in range(0, bytes_sent):
+    if BAD_PACKET == False and NEXT_PACKET == SEQ_NUM:
+        for i in range(0, BYTES_SENT):
             IMG_BUFFER.write(list(PAYLOAD)[i])
         NEXT_PACKET += 1
-    if LAST_PKT == True:
+    if LAST_PKT == True and BAD_PACKET == False:
         for proc in psutil.process_iter():
             if proc.name == "display":
                 proc.kill()
@@ -46,3 +53,8 @@ while True:
         JPG_FILE.write(IMG_BUFFER.getvalue())
         JPG_FILE.close()
         Image.open(StringIO(IMG_BUFFER.getvalue())).show()
+    elif LAST_PKT == True and BAD_PACKET == True:
+        NEXT_PACKET = 0
+        BAD_PACKET = False
+        syslog.syslog(syslog.LOG_ERR,
+            "Received last packet afer bad packets.  Resetting")
