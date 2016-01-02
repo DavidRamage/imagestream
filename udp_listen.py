1bdda960d78d99642ed62f21f15327aefcac7e7d
David Ramage <davidramage@github.com>
2016-01-01 17:59:05 -0800

    fix issues 2 and 3

--- udp_listen.py
+++ udp_listen.py
@@ -3,16 +3,73 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 import struct
 import time
+import os
 from io import BytesIO
 import binascii
 import syslog
-import Image
-from cStringIO import StringIO
-import psutil
+import sys
+import getopt
+from SimpleCV import Image
 SOCK = socket(AF_INET, SOCK_DGRAM)
 HOST = '255.255.255.255'
 PORT = 31337
 PKT_STRUCT = struct.Struct("> B l I H 457s")
+<<<<<<< HEAD
+
+def help_out():
+    """Display help about the program"""
+    print "./udp_listen.py (-s -d -h)"
+    print "-s do not save file"
+    print "-d do not display image"
+    print "-h this help"
+    sys.exit()
+
+if __name__ == '__main__':
+    opts, args = getopt.getopt(sys.argv[1:], "dsh")
+    DISPLAY = True
+    SAVE = True
+    for opt in opts:
+        if opt[0] == '-h':
+            help_out()
+        if opt[0] == '-s':
+            SAVE = False
+        if opt[0] == '-d':
+            DISPLAY = False
+    try:
+        SOCK.bind((HOST, PORT))
+    except Exception as ex:
+        syslog.syslog(syslog.LOG_ERR, "Unable to bind to socket due to %s" %\
+        str(ex))
+    BAD_PACKET = False
+    NEXT_PACKET = 0
+    while True:
+        (LAST_PKT, CRC_32, SEQ_NUM, bytes_sent, PAYLOAD) =\
+            PKT_STRUCT.unpack(SOCK.recvfrom(512)[0])
+        if SEQ_NUM == 0:
+            BAD_PACKET = False
+            IMG_BUFFER = BytesIO()
+        if NEXT_PACKET != SEQ_NUM:
+            BAD_PACKET = True
+            syslog.syslog(syslog.LOG_DEBUG, "Missed packet sequence number")
+        if binascii.crc32(PAYLOAD) != CRC_32:
+            BAD_PACKET = True
+            syslog.syslog(syslog.LOG_DEBUG, "Bad PAYLOAD CRC in packet")
+        if BAD_PACKET == False:
+            for i in range(0, bytes_sent):
+                IMG_BUFFER.write(list(PAYLOAD)[i])
+            NEXT_PACKET += 1
+        if LAST_PKT == True:
+            NEXT_PACKET = 0
+            FILE_NAME = str(int(time.time())) + '.jpg'
+            JPG_FILE = open(FILE_NAME, 'wb')
+            JPG_FILE.write(IMG_BUFFER.getvalue())
+            JPG_FILE.close()
+            if DISPLAY:
+                img = Image(FILE_NAME)
+                img.show()
+            if SAVE == False:
+                os.unlink(FILE_NAME)
+=======
 try:
     SOCK.bind((HOST, PORT))
 except Exception as ex:
@@ -58,3 +115,4 @@ while True:
         BAD_PACKET = False
         syslog.syslog(syslog.LOG_ERR,
             "Received last packet afer bad packets.  Resetting")
+>>>>>>> 978c811362e9a2947741249d19bac3a7a378b04a
